a663c5b467c1ab13fa9402fa16a766ed6d0f31f8
Elian Melo <elianmelomorais@gmail.com>
2026-03-22 01:34:47 -0300

    Read input using ECS

---
+++ Assets/_Project/Scripts/ECS/InputSystem.cs
@@ -0,0 +1,25 @@
+using Unity.Entities;
+using UnityEngine;
+
+public partial class InputSystem : SystemBase
+{
+    private InputSystem_Actions controls;
+
+    protected override void OnCreate()
+    {
+        if(!SystemAPI.TryGetSingleton<InputComponent>(out InputComponent input))
+        {
+            EntityManager.CreateEntity(typeof(InputComponent));
+        }
+        controls = new InputSystem_Actions();
+        controls.Enable();
+    }
+
+    protected override void OnUpdate() { 
+        Vector2 moveVector = controls.Player.Movement.ReadValue<Vector2>();
+        Vector2 mousePosition = controls.Player.MousePosition.ReadValue<Vector2>();
+        bool isPressingLMB = controls.Player.Shoot.ReadValue<float>() == 1 ? true : false;
+
+        SystemAPI.SetSingleton(new InputComponent { mousePos = mousePosition, movement = moveVector, pressingLMB = isPressingLMB });
+    }
+}
